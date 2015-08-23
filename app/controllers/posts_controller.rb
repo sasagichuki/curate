bcaf2ceed0628c0b8c695d10ede5d8297bdb6fc2
Charles Gichuki <charles@sprout.co.ke>
2015-08-23 16:47:02 +0300

    Added Devise user model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	before_action :find_post, only: [:show, :edit,:destroy,:update]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@post = Post.all.order("created_at DESC")
@@ -9,10 +10,10 @@ class PostsController < ApplicationController
 
 	end
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 
 		if @post.save
 			redirect_to @post
