6234fdbeadd987179435ebb5c279acb1bb16de1e
Charles Gichuki <charles@sprout.co.ke>
2015-08-23 19:34:45 +0300

    Added Paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,6 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 	def post_params
-		params.require(:post).permit(:title, :link, :description)
+		params.require(:post).permit(:title, :link, :description, :image)
 	end
 end
